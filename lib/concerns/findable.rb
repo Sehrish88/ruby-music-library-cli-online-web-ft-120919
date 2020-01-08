8f2c5e2482d1cb7c23609dac0bc09a5a22b745c9
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-08 21:43:03 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,3 +1,14 @@
 module Concerns::Findable
+  module Concerns::Findable
+  def find_by_name(name)	  def find_by_name(name)
+    self.all.detect{|s| s.name == name}	    self.all.each {|song| return song if song.name == name}
+    nil
+  end	  end
+
+
+  def find_or_create_by_name(name)	  def find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)	    self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+  end	  end
+end	end
  
 end	
