f1c0e3abd9ab2f752043c23d2fc20eab77a599fe
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-08 21:45:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,11 @@
 class Song 
   all = [] 
-  attr_accessor :name, :artist 
+  attr_accessor :name, :artist, :genre 
   
   def initialize 
       @name = name
       @artist = artist 
+      @genre = genre 
       
       @@all << self 
   end 
